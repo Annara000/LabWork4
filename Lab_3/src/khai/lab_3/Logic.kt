99d2efa53fb340d81f3caf62e40b1c8ba8e60622
Annara000 <annamail.000@mail.ru>
2017-11-07 18:50:23 +0200

    Delete Logic.kt

--- Lab_3/src/khai/lab_3/Logic.kt
+++
@@ -1,8 +0,0 @@
-package khai.lab_3
-
-interface LogicMinIndex {
-    /**
-     * Даны три числа. Выдать порядковый номер меньшего из них.
-     */
-    fun min_index(a: Double, b: Double, c: Double): Int
-}
