7d7764ca3c486297e1598baace43a429a0916285
Annara000 <annamail.000@mail.ru>
2017-11-07 18:50:38 +0200

    Delete Array.kt

--- Lab_3/src/khai/lab_3/Array.kt
+++
@@ -1,9 +0,0 @@
-package khai.lab_3
-
-interface ArrayExceptKL {
-    /**
-     * Найти сумму всех элементов массива,
-     * кроме элементов с номерами от K до L.
-     */
-    fun except_kl(A: Array<Double>, K: Int, L: Int)
-}
